1a16d8435e1bc370721358502748cf5425b95089
sonic160 <zhiguo.zeng@centralesupelec.fr>
2024-09-26 22:55:51 +0200

    generate data for stuck failure mode.

--- scripts/test_failure_simulation.m
+++ scripts/test_failure_simulation.m
@@ -2,6 +2,8 @@ clear; clc; close all;
 
 addpath('../digital_model_complete/')
 addpath('../digital_model_kinematics/')
+warning('off')
+
 
 % Define parameters of the simulink model.
 mdl_name = 'main3_armpi_fpv.slx';
@@ -19,13 +21,59 @@ end_time_value_in_seconds = (len_time_series-1)*simulation_time/len_time_series;
 % Get the time steps of the sequence.
 time_stamps = 0:0.01:end_time_value_in_seconds;
 
-% Generate a random pick operation.
-traj_type = 'pick';
-n_traj = 1;
-input_motor_commands = generateTrajectories(traj_type, n_traj, time_stamps);
+% Define the simulaion set-up.
+traj_type_list = {'random_move'}; % How many trajectory types to consider.
+label_list = {'Healthy', 'Motor_1_Stuck', 'Motor_2_Stuck', 'Motor_3_Stuck', 'Motor_4_Stuck', 'Motor_5_Stuck'}; % Define the labels to be simulated.
+n_label = length(label_list); % Get the number of labels.
+n_traj_per_label = 2; % Define the number of trajectory simulated per label.
+visualization = 0; % Turn off the visualization.
 
-failure_type = 0; % No failure
-visualization = 1;
-[joint_cmds, joint_resps, traj_cmd, traj_resp] = run_simulation(input_motor_commands, failure_type, mdl_name, visualization);
+root_dir = 'RobotPdMDataset/';
 
+% Start simulation loop.
+% Loop over each class. Creat a subfolder per class.
+for i = 1:n_label 
+    % Retrive the label.
+    label = label_list{i};
+    failure_type = i-1; % Index for the class.
 
+    % Check if the subfolder exists
+    sub_folder = [root_dir label];
+    if ~exist(sub_folder, 'dir')
+        % If the folder does not exist, create it
+        mkdir(sub_folder);
+    end
+
+    counter = 0; % Define a counter to name the data files in a given subfolder.
+    
+    % Loop over the required trajectory type.
+    for j = 1:length(traj_type_list)
+        % Get the current trajectory type.
+        traj_type = traj_type_list{j};
+        % Generate n_traj_per_label trajectories.
+        input_motor_commands = generateTrajectories(traj_type, n_traj_per_label, time_stamps);
+
+        % Simulate the needed trajectories.
+        for k = 1:n_traj_per_label
+            fprintf('i: %d/%d; j: %d/%d; k: %d/%d\n', i, n_label, j, length(traj_type_list), k, n_traj_per_label);
+
+            % Generate filename
+            counter = counter + 1;
+            filePath = fullfile(sub_folder, ['dataset_' num2str(counter)]);
+
+            % Run simulation
+            [joint_cmds, joint_resps, traj_cmd, traj_resp] = run_simulation(input_motor_commands{k}, failure_type, mdl_name, visualization);
+            
+            % Save the trajectories as dataset.
+            dataset = zeros(len_time_series, 6);
+            dataset(:, 1:3) = traj_cmd;
+            dataset(:, 4:6) = traj_resp;
+            save(filePath, 'dataset');
+
+            % Log also the command and response on the component level.
+            hidden_dataset = {joint_cmds, joint_resps};
+            save(fullfile(sub_folder, ['hidden_dataset_' num2str(counter)]), 'hidden_dataset');
+        end
+    end
+    
+end
